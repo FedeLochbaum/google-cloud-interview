2ddb071d54d0c95ba462ac48cf1c5f571c555018
Federico Lochbaum <federico.lochbaum@gmail.com>
2023-09-25 10:54:32 -0300

    champagne_tower

--- leet_code/champagne_tower.py
+++ leet_code/champagne_tower.py
@@ -38,3 +38,19 @@
 # 0 <= poured <= 109
 # 0 <= query_glass <= query_row < 100
 
+def champagne_tower(poured, row, glass):
+  tower = [[0] * i for i in range(1, row + 2)]
+  tower[0][0] = poured
+
+  for i in range(row):
+    for j in range(len(tower[i])):
+      prev_excess = (tower[i][j] - 1) / 2
+      if prev_excess > 0:
+        tower[i + 1][j] += prev_excess
+        tower[i + 1][j + 1] += prev_excess
+
+  return min(1.0, tower[row][glass])
+
+print(champagne_tower(1, 1, 1))
+print(champagne_tower(2, 1, 1))
+print(champagne_tower(100000009, 33, 17))
